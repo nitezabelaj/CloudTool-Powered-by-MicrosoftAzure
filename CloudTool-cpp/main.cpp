233eaa5e50b4384a64899b5b1a74cfca84c2fbd6
jonaguri <jonaguri484@gmail.com>
2025-12-18 18:48:42 +0100

    Update main.cpp
    Shtimi i funksionit për shfaqjen e regjioneve cloud dhe shtimi i funksionit për shfaqjen e shërbimeve cloud

--- CloudTool-cpp/main.cpp
+++ CloudTool-cpp/main.cpp
@@ -8,4 +8,22 @@ void pershendetje() {
 int main() {
     pershendetje();
     return 0;
+
+}
+
+#include <iostream>
+using namespace std;
+
+void shfaqRegjionet() {
+    cout << "Regjionet Cloud:\n";
+    cout << "- Europe\n";
+    cout << "- USA\n";
+    cout << "- Asia\n";
+}
+
+void shfaqSherbimet() {
+    cout << "\nSherbimet Cloud:\n";
+    cout << "- Storage\n";
+    cout << "- Virtual Machines\n";
+    cout << "- Databases\n";
 }
